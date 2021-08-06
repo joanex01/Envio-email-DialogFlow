fc3d354254e55dee1e8096b728dc5479ace64fe0
joanex01 <70920904+joanex01@users.noreply.github.com>
2021-08-06 15:43:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 //const bodyParser = require('body-parser')
 const { request } = require('express')
 const {WebhookClient} = require('dialogflow-fulfillment');
+const sendmail = require('sendmail');
 const nodemailer = require('nodemailer');
 
 const app = express()
